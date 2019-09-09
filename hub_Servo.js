ad6ef41b63aeacbefaab5d49156d9d2fb6ae285f
Andreas Urben <urben77@bluewin.ch>
2019-09-09 19:54:11 +0200

    motorshild activated

--- hub_Servo.js
+++ hub_Servo.js
@@ -147,7 +147,9 @@ var dutyCicle=0;
 // Toggle the state of the LED on GPIO #7 every 200ms.
 // Here synchronous methods are used. Asynchronous methods are also available.
 // iv=setInterval(drive,3);
-iv=setInterval(driveServo,3);
+//iv=setInterval(driveServo,3);
+iv=setInterval(driveMotorShield,3);
+
 
 function setTarget() {
   step_target=100;
