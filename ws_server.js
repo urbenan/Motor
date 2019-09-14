032f79ffcb64ff55464d1111ea6486102dabb038
Andreas Urben <urben77@bluewin.ch>
2019-09-14 15:27:28 +0200

    socket test added

---
+++ ws_server.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const https = require('https');
+const WebSocket = require('ws');
+ 
+
+const server = https.createServer({
+  cert: fs.readFileSync('cert.pem'),
+  key: fs.readFileSync('key.pem')
+});
+const wss = new WebSocket.Server({ server });
+ 
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+ 
+  ws.send('something');
+});
+ 
+server.listen(8080);
