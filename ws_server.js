e8921619b304606968522b3e5160d90f8c95473c
Andreas Urben <urben77@bluewin.ch>
2019-09-14 15:39:50 +0200

    test ws

--- ws_server.js
+++ ws_server.js
@@ -1,12 +1,34 @@
-const fs = require('fs');
 const https = require('https');
+const fs = require('fs');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+var index = fs.readFileSync('sensor.html');
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+var app = connect();
+
+app.use(serveStatic(__dirname))
+server=https.createServer(options, app).listen(8080);
+
+
+
+
+// const fs = require('fs');
+// const https = require('https');
 const WebSocket = require('ws');
  
-
+/*
 const server = https.createServer({
   cert: fs.readFileSync('cert.pem'),
   key: fs.readFileSync('key.pem')
 });
+*/
+
 const wss = new WebSocket.Server({ server });
  
 wss.on('connection', function connection(ws) {
